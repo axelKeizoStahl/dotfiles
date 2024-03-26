e636b2a12c0d54ca365e303ba8bb7515845131f6
axelKeizoStahl <axelkeizo@gmail.com>
2024-03-26 17:11:35 -0400

    fix in files

--- scripts/curr_window.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-WORKSPACE_INFO=$(swaymsg -t get_workspaces)
-WORKSPACE_NUM=$(echo "$WORKSPACE_INFO" | jq -r '.[] | select(.focused==true) | .name' | grep -o -E '^[0-9]+')
-echo $WORKSPACE_NUM
--- sway/scripts/window_logo.sh
+++ sway/scripts/window_logo.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
 
-CURR_WINDOW="\"$1\""
+curr_window="\"$1\""
 
-CURR_APPLICATION=$(swaymsg -t get_workspaces | jq ".[] | select(.name==$CURR_WINDOW) | .representation" | grep -oP '\[\K[^\s\]]+')
-# NEXT STEPS
-# look for app.desktop file in /usr/applications/
-# use the Icon field to get the root of the file name
-# look for root.png in /usr/share/icons/hicolor/256x256/apps/
-echo $CURR_APPLICATION
+curr_application=$(swaymsg -t get_workspaces | jq ".[] | select(.name==$curr_window) | .representation" | grep -oP '\[\K[^\s\]]+')
+
+if [ $? -eq 0 ]
+then 
+    echo "Current (main) application in workspace $curr_window: $curr_application"
+    echo "Logo path for $curr_application: /usr/share/icons/hicolor/256x256/apps/$curr_application.png"
+else
+    echo "Currently no application opened in workspace $curr_window"
+fi
